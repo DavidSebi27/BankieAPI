17d76b3a356bc2a97694c3ec7d3eef4ea7202fde
Dávid Sebestyén <730630@student.inholland.nl>
2026-04-16 13:02:57 +0200

    Project creation and initialization
    Basic springboot frame pulled up, dependencies added

---
+++ bankie-api/src/main/java/com/bankie/bankie_api/BankieApiApplication.java
@@ -0,0 +1,13 @@
+package com.bankie.bankie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankieApiApplication.class, args);
+	}
+
+}
---
+++ bankie-api/src/test/java/com/bankie/bankie_api/BankieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankie.bankie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
