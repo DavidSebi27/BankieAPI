4c3d3675353b6ab98428d175c261b0092b3aa783
Hanish Suresh <cloutnorcal@gmail.com>
2026-05-16 07:39:57 +0200

    few fixes

--- src/main/java/com/bankie/bankie_api/controller/TransactionController.java
+++ src/main/java/com/bankie/bankie_api/controller/TransactionController.java
@@ -1,6 +1,5 @@
 package com.bankie.bankie_api.controller;
 
-import com.bankie.bankie_api.dto.request.TransferRequestDTO;
 import com.bankie.bankie_api.dto.response.TransactionResponseDTO;
 import com.bankie.bankie_api.service.TransactionService;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transactions")
@@ -24,13 +22,5 @@ public class TransactionController {
     public Page<TransactionResponseDTO> list(
             @PageableDefault(size = 20, sort = "timestamp", direction = Sort.Direction.DESC) Pageable pageable) {
         return transactionService.findAll(pageable);
-    }
-
-    @PostMapping
-    public ResponseEntity<TransactionResponseDTO> transfer(
-            @RequestBody TransferRequestDTO dto,
-            Authentication authentication) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(transactionService.performTransfer(dto, authentication));
     }
 }
